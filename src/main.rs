92c13e1687034de9c04a46ee54a8c240322486e9
Johnny T. Vestergaard <jtv@jrtv.dk>
2022-01-01 22:32:47 +0100

    fix: error in generated main() fn when using Bevy git version

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
 
 fn main() {
-    {% if bevy_version == "git" %}App::app(){%else%}App::build(){%endif%}
+    {% if bevy_version == "git" %}App::new(){%else%}App::build(){%endif%}
         .add_system(hello_world.system())
         .run();
 }
