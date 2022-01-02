37bc916b4bf00cb456c1e6cc994296093cbbae77
Johnny T. Vestergaard <jtv@jrtv.dk>
2022-01-02 14:15:18 +0100

    feat: improve example code

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,49 @@
-#[cfg(test)]
-mod test {
-    use super::*;
+use bevy::prelude::*;
 
-    #[test]
-    fn name() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+pub struct HelloPlugin;
+
+{% if bevy_version == "git" %}#[derive(Component)]
+{% endif %}pub struct Person;
+
+{% if bevy_version == "git" %}#[derive(Component)]
+{% endif %}pub struct Name(String);
+
+struct GreetTimer(Timer);
+{% if bevy_version == "git" %}
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people)
+            .add_system(greet_people);
+    }
+}{% else %}
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people.system())
+            .add_system(greet_people.system());
+    }
+}{% endif %}
+
+fn add_people(mut commands: Commands) {
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Elaina Proctor".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Renzo Hume".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Zayna Nieves".to_string()));
+}
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}!", name.0);
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
+use {{crate_name}}::*;
 use bevy::prelude::*;
 
-fn main() {
-    {% if bevy_version == "git" %}App::new(){%else%}App::build(){%endif%}
-        .add_system(hello_world{% if bevy_version != "git" %}.system(){%endif%})
-        .run();
-}
 
-fn hello_world() {
-    println!("hello world!");
+fn main() {
+    {% if bevy_version == "git" %}App::new(){% else %}App::build(){% endif %}
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
 }
