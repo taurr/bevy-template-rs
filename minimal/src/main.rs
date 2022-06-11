4345a3267f2dbe86fcf7cc1ffb890138c46efd3f
laundmo <laurinschmidt2001@gmail.com>
2022-06-12 00:48:05 +0200

    minimal template

---
+++ minimal/src/main.rs
@@ -0,0 +1,12 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    
+}
